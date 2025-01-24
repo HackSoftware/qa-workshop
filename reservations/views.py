b6d426d37860fd785cbd22e6a0b43f1310950604
Ivaylo Bachvarov <ivaylo@hacksoft.io>
2025-01-24 08:13:50 +0200

    Add the nuke API

--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+from .models import Property, Reservation, Guest
+
+
+@csrf_exempt
+def nuke(request):
+    if request.method != "POST":
+        return JsonResponse({'message': "Only POST requests are allowed!"}, status=405)
+
+    user = request.user
+
+    if user.is_anonymous:
+        return JsonResponse({'message': "You must be logged in to nuke your data!"}, status=403)
+
+    Property.objects.filter(owner=user).delete()
+    Reservation.objects.filter(owner=user).delete()
+    Guest.objects.filter(owner=user).delete()
+
+    return JsonResponse({'message': "Data nuked!"})
